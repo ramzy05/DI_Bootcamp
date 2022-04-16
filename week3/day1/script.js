920270357bc137240294da897b54800ae1e03865
ramzy <ramsesaguegang@gmail.com>
2022-04-16 12:04:18 +0200

    week3 begin

---
+++ week3/day1/script.js
@@ -0,0 +1,10 @@
+//1
+let adressNumber = "Je vis à Mendong", adressStreet = "Rue 300", country = "Cameroun";
+
+//2
+
+let globalAdress = adressNumber.concat(' ', adressStreet, ', au', country);
+
+//3
+
+console.log(globalAdress);
