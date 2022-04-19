716739bbafd4fedac9ad6bd02ca9fb5bedbd2ce4
ramzy <ramsesaguegang@gmail.com>
2022-04-19 03:19:54 +0200

    first daily challenge of js end with mandatory exercise

--- week3/day1/script.js
+++ week3/day1/script.js
@@ -8,3 +8,20 @@ let globalAdress = adressNumber.concat(' ', adressStreet, ', au', country);
 //3
 
 console.log(globalAdress);
+
+//exercise 2
+let yearOfBirth = 1970, futureYear = 2035;
+console.log("I will be "+ (futureYear - yearOfBirth) +" in " + yearOfBirth + "." );
+
+
+// exo 3
+
+let pets = ['cat','dog','fish','rabbit','cow'];
+console.log(pets[1]);
+
+
+pets.push(' horse');
+pets.splice(pets.length-3, 1);
+
+console.log(pets.length)
+console.log(pets)
