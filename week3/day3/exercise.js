21da8eef94192494cbb603738abf40b6ce4d6a92
ramzy <ramsesaguegang@gmail.com>
2022-04-20 20:11:10 +0200

    end of week3 day3

---
+++ week3/day3/exercise.js
@@ -0,0 +1,57 @@
+//conditionals
+//ex1<>
+
+/* let userAge = parseInt(prompt('Enter your age please'));
+if(userAge < 18){
+  alert('Sorry, you are too young to drive this car. Powering off');
+}else if(userAge === 18){
+  alert('Congratulations on your first year of driving. Enjoy the ride!');
+}else{
+  alert('Powering On. Enjoy the ride!'); 
+}*/
+//end1
+
+//ex2<>
+/* let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+    break;
+  case 4://the instruction of this case are not going to be executed because 2+2= 4 
+    alert( 'Exactly!' );// a === 4 :true, so we display 'Exactly'
+    break;
+  case 5:
+    alert( 'Too large' );//the instruction of this case are not going to be executed because 2+2= 4
+    break;
+  default:
+    alert( "I don't know such values" );//this instruction is not going to run because a the case n°2 we have break the switch statement 
+} */
+
+//end2
+
+//end conditionals
+
+//intro  to object
+let user = {
+  username : 'ramzy',
+  password : 'password'
+};
+
+let users = [user];
+let newsfeed = [
+  {
+    username : 'junior12',
+    timeline : '12/01/2021'
+  },
+  {
+    username : 'ram8',
+    timeline : '01/11/2001'
+  },
+  {
+    username : 'tom32',
+    timeline : '19/01/1999'
+  }
+];
+
+//end intro to object
