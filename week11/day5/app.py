df3c823e9c9151a0e178cda4eadd31fb1d9527d2
ramzy05 <ramsesfeukeng@mail.com>
2022-06-14 18:08:37 +0000

    message

--- week11/day5/app.py
+++ week11/day5/app.py
@@ -1,7 +1,33 @@
 import flask
+from flask import render_template, redirect, render_template_string
 from products_data import *
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+  return render_template('index.html')
+
+@app.route('/products')
+def see_all_products():
+  return render_template('products.html',products = all_products)
+
+@app.route('/products/<product_id>')
+def see_more_product(product_id):
+  return render_template('see_more.html',product = retrieve_requested_product(product_id))
+d
+@app.route('/cart')
+def see_cart():
+  return render_template('cart.html', products=all_products, totals=[round(sum([p['Price'] for p in all_products]),2), len(all_products)])
+# cart_item = [] 
+@app.route('/add_prod/<product_id>')
+def add_to_cart(product_id):
+  add_cart(product_id)
+  return 'bon'
+
+@app.route('/delete_prod/<product_id>')
+def del_to_cart(product_id):
+  all_products.remove(retrieve_requested_product(product_id))
+  return 'bad'
+
+if __name__ == "__main__":
+  app.run(debug=True)
--- week11/day5/products_data.py
+++ week11/day5/products_data.py
@@ -3,14 +3,34 @@ import json
 
 def retrieve_all_products():
   with open ('products.json') as f:
-    return json.load(f)
+    return list(json.load(f))
 
 all_products = retrieve_all_products()
 def retrieve_requested_product(product_id):
   with open ('products.json') as f:
     all_products = json.load(f)
-  return [product for product in all_products if product['ProductId'] == product_id]
+  return [product for product in all_products if product['ProductId'] == product_id][0]
 
-requested_product = retrieve_requested_product('HT-1107')
+def add_cart(product_id):
+  with open ('cart.json') as f:
+    cart_item = json.load(f)
+  print(cart_item)
+    # cart_item.append(retrieve_requested_product(product_id))
 
-print(requested_product)
+  # with open ('cart.json', 'w') as f:
+    # json.dump(cart_item,f)
+
+def del_cart(product_id):
+  with open ('cart.json') as f:
+    cart_item = json.load(f)
+  cart_item = [product for product in cart_item if product['ProductId'] != product_id][0]  
+  with open ('cart.json', 'w') as f:
+    json.dump(cart_item,f)
+
+  with open ('cart.json', 'w') as f:
+    f.write(json.dump(cart_item))
+
+if __name__ == '__main__':
+
+  requested_product = retrieve_requested_product('HT-1107')
+  print(requested_product)
